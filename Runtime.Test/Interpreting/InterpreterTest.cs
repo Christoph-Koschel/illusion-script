372c18f5975d0163e7766874ef1eeca346a70486
Sleepy-Reacon <63058519+Sleepy-Reacon@users.noreply.github.com>
2022-04-16 13:14:42 +0200

    Added InterpreterTest.cs

--- Runtime.Test/Interpreting/InterpreterTest.cs
+++ Runtime.Test/Interpreting/InterpreterTest.cs
@@ -1,7 +1,88 @@
-﻿namespace Runtime.Test.Interpreting
+﻿using System.Collections.Generic;
+using System.Linq;
+using IllusionScript.Runtime.Interface;
+using IllusionScript.Runtime.Parsing;
+using Xunit;
+
+namespace Runtime.Test.Interpreting
 {
     public class InterpreterTest
     {
+        [Theory]
+        [InlineData("1", 1)]
+        [InlineData("+1", 1)]
+        [InlineData("-1", -1)]
+        [InlineData("~1", -2)]
+        [InlineData("14 + 12", 26)]
+        [InlineData("12 - 3", 9)]
+        [InlineData("4 * 2", 8)]
+        [InlineData("9 / 3", 3)]
+        [InlineData("(10)", 10)]
+        [InlineData("12 == 3", false)]
+        [InlineData("3 == 3", true)]
+        [InlineData("12 != 3", true)]
+        [InlineData("3 != 3", false)]
+        [InlineData("3 < 4", true)]
+        [InlineData("5 < 4", false)]
+        [InlineData("4 <= 4", true)]
+        [InlineData("4 <= 5", true)]
+        [InlineData("5 <= 4", false)]
+        [InlineData("4 > 3", true)]
+        [InlineData("4 > 5", false)]
+        [InlineData("4 >= 4", true)]
+        [InlineData("5 >= 4", true)]
+        [InlineData("4 >= 5", false)]
+        [InlineData("1 | 2", 3)]
+        [InlineData("1 | 0", 1)]
+        [InlineData("1 & 3", 1)]
+        [InlineData("1 & 0", 0)]
+        [InlineData("1 ^ 0", 1)]
+        [InlineData("0 ^ 1", 1)]
+        [InlineData("1 ^ 3", 2)]
+        [InlineData("false == false", true)]
+        [InlineData("true == false", false)]
+        [InlineData("false != false", false)]
+        [InlineData("true != false", true)]
+        [InlineData("true && true", true)]
+        [InlineData("false || false", false)]
+        [InlineData("false | false", false)]
+        [InlineData("false | true", true)]
+        [InlineData("true | false", true)]
+        [InlineData("true | true", true)]
+        [InlineData("false & false", false)]
+        [InlineData("false & true", false)]
+        [InlineData("true & false", false)]
+        [InlineData("true & true", true)]
+        [InlineData("false ^ false", false)]
+        [InlineData("true ^ false", true)]
+        [InlineData("false ^ true", true)]
+        [InlineData("true ^ true", false)]
+        [InlineData("true", true)]
+        [InlineData("false", false)]
+        [InlineData("!true", false)]
+        [InlineData("!false", true)]
+        [InlineData("\"test\"", "test")]
+        [InlineData("\"te\\\"st\"", "te\"st")]
+        [InlineData("\"test\" == \"test\"", true)]
+        [InlineData("\"test\" != \"test\"", false)]
+        [InlineData("\"test\" == \"abc\"", false)]
+        [InlineData("\"test\" != \"abc\"", true)]
+        [InlineData("\"test\" + \"abc\"", "testabc")]
         
+        public void Evaluator_Computes_CorrectValues(string text, object expectedValue)
+        {
+            AssertValue(text, expectedValue);
+        }
+
+        private static void AssertValue(string text, object expectedValue)
+        {
+            Compilation compilation = SyntaxTree.Parse(text);
+            Assert.False(compilation.diagnostics.Any());
+
+            InterpreterResult result = compilation.Interpret();
+
+            Assert.False(result.diagnostic.Any());
+            Assert.Equal(expectedValue, result.value);
+        }
     }
 }
