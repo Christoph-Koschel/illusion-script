23fa8cd6357a8bfb53c897b48f0c6c8918e9c9d6
Christoph Koschel <63058519+Sleepy-Reacon@users.noreply.github.com>
2022-05-07 14:11:29 +0200

    Init compiler to PHP8

---
+++ php8/Compiler.cs
@@ -0,0 +1,22 @@
+﻿using IllusionScript.Runtime.Compiling;
+
+namespace IllusionScript.Compiler.PHP8
+{
+    public class Compiler : CompilerConnector
+    {
+        public Compiler(string baseDir) : base(baseDir)
+        {
+            
+        }
+
+        public override void BuildOutput()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public override void Build()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
